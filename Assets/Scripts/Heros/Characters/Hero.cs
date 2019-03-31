e1861fd28d352a36cd5eba9852396d1243c23b57
Rui Martins <vandroste@gmail.com>
2019-03-31 00:23:16 +0000

    Create Hero abstract class and specific Hero FPessoa class, map the respective melee and ranged attack controller buttons, animation of weapons through code in progress

---
+++ Assets/Scripts/Heros/Characters/Hero.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Hero : MonoBehaviour
+{
+    [SerializeField] private float maximumHealth;
+
+    private HealthBar healthBar;
+
+    private float currentHealth;
+
+    // Input
+    private bool isL1InUse;
+    private bool isL2InUse;
+    private bool isR1InUse;
+    private bool isR2InUse;
+
+    [Header("Weapons")]
+    [SerializeField] protected Collider meleeWeapon;
+    [SerializeField] protected Collider rangeWeapon;
+
+    protected abstract void RangeAttack();
+    protected abstract void MeleeAttack();
+    protected abstract void RegularAbility();
+    protected abstract void UltimateAbility();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar = GetComponent<HealthBar>();
+        healthBar.MaximumHealth = maximumHealth;
+
+        isL1InUse = false;
+        isL2InUse = false;
+        isR1InUse = false;
+        isR2InUse = false;
+    }
+
+    private void Update()
+    {
+        LaunchAttack();
+    }
+
+    private void LaunchAttack()
+    {
+        if (Input.GetAxis("Melee") != 0)
+        {
+            if (!isR1InUse)
+            {
+                Debug.Log("Melee");
+                MeleeAttack();
+                isR1InUse = true;
+            }
+        }
+        else
+            isR1InUse = false;
+
+        if (Input.GetAxis("Range") != 0)
+        {
+            if (!isL1InUse)
+            {
+                Debug.Log("Range");
+                RangeAttack();
+                isL1InUse = true;
+            }
+        }
+        else
+            isL1InUse = false;
+    }
+
+    protected void Attack(Collider col)
+    {
+        /* Return all colliders that are overlapping our attacking collider
+         * and have a HitBox layer mask */
+        Collider[] enemyCols =
+            Physics.OverlapBox(col.bounds.center, col.bounds.extents,
+            col.transform.rotation, LayerMask.GetMask("Hitbox"));
+
+        foreach (Collider c in enemyCols)
+        {
+            // Ignore our own body
+            if (c.transform == transform)
+            {
+                Debug.Log("ATAKEIII");
+                continue;
+            }
+            // Attack
+            else
+                ApplyDamage(c);
+        }
+    }
+
+    private void ApplyDamage(Collider col)
+    {
+        Debug.Log($"I've hit the {col.name}");
+        col.SendMessageUpwards("SetHealthbarSize", 10f);
+    }
+}
---
+++ Assets/Scripts/Heros/Characters/Hero_FernandoPessoa.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero_FernandoPessoa : Hero
+{
+    [Header("Melee")]
+    [SerializeField] private float maxMeleeDistance;
+    [SerializeField] private float meleeAttackSpeed;
+    [Header("Ranged")]
+    [SerializeField] private float maxRangeDistance;
+    [SerializeField] private float rangedAttackSpeed;
+
+    protected override void MeleeAttack()
+    {
+        Attack(meleeWeapon);
+        StartCoroutine(SwingSword
+            (meleeWeapon.transform.position + new Vector3(0, 0, maxMeleeDistance)));
+    }
+
+    protected override void RangeAttack()
+    {
+        Attack(rangeWeapon);
+    }
+
+    protected override void RegularAbility()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void UltimateAbility()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // NEED TO FIX DIS SHIT
+    IEnumerator SwingSword(Vector3 endPos)
+    {
+        float elapsedTime = 0;
+        Vector3 startingPos = meleeWeapon.transform.position;
+
+        while (elapsedTime < meleeAttackSpeed)
+        {
+            elapsedTime += Time.deltaTime;
+            meleeWeapon.transform.position = Vector3.Lerp(startingPos, endPos, meleeAttackSpeed);
+            yield return null;
+        }
+    }
+}
