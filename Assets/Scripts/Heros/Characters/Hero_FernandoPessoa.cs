fe5edb6546d9ca14511208722c87af32223d1e52
Rui Martins <vandroste@gmail.com>
2019-04-02 14:12:35 +0100

    Fix melee attack. Add range attack

--- Assets/Scripts/Heros/Characters/Hero_FernandoPessoa.cs
+++ Assets/Scripts/Heros/Characters/Hero_FernandoPessoa.cs
@@ -11,32 +11,58 @@ public class Hero_FernandoPessoa : Hero
     [SerializeField] private float maxRangeDistance;
     [SerializeField] private float rangedAttackSpeed;
 
+    private Vector3 startingPos;
+    private Vector3 targetPos;
+    
     private float timeElapsed;
+    private bool attackDistanceFlag;
 
     private void Start()
     {
         timeElapsed = 0;
+        attackDistanceFlag = false;
     }
 
     protected override void MeleeAttack()
     {
-        Vector3 startingPos = meleeWeapon.transform.localPosition;
+        if (!attackDistanceFlag)
+        {
+            startingPos = meleeWeapon.transform.localPosition;
+            targetPos = meleeWeapon.transform.localPosition + new Vector3(0, 0, maxMeleeDistance);
+            attackDistanceFlag = true;
+        }
 
-        meleeWeapon.transform.localPosition 
-            = Vector3.Lerp(startingPos, startingPos += new Vector3(0, 0, maxMeleeDistance), meleeAttackSpeed * Time.deltaTime);
-
+        meleeWeapon.transform.localPosition = Vector3.Lerp(startingPos, targetPos, Mathf.InverseLerp(0, meleeAttackSpeed, timeElapsed));
         timeElapsed += Time.deltaTime;
+
         if (timeElapsed >= meleeAttackSpeed)
         {
             isR1InUse = false;
             timeElapsed = 0;
+            attackDistanceFlag = false;
+            meleeWeapon.transform.localPosition = startingPos;
         }
-
-        //StartCoroutine(SwingSword());
     }
 
     protected override void RangeAttack()
     {
+        if (!attackDistanceFlag)
+        {
+            startingPos = rangeWeapon.transform.localPosition;
+            targetPos = rangeWeapon.transform.localPosition + new Vector3(0, 0, maxRangeDistance);
+            attackDistanceFlag = true;
+        }
+
+        rangeWeapon.transform.localPosition = Vector3.Lerp(startingPos, targetPos, Mathf.InverseLerp(0, rangedAttackSpeed, timeElapsed));
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed >= rangedAttackSpeed)
+        {
+            isL1InUse = false;
+            timeElapsed = 0;
+            attackDistanceFlag = false;
+            rangeWeapon.transform.localPosition = startingPos;
+        }
     }
 
     protected override void RegularAbility()
@@ -45,20 +71,5 @@ public class Hero_FernandoPessoa : Hero
 
     protected override void UltimateAbility()
     {
-    }
-
-    // NEED TO FIX DIS SHIT
-    IEnumerator SwingSword()
-    {
-        float elapsedTime = 0;
-        Vector3 startingPos = meleeWeapon.transform.localPosition;
-
-        while (elapsedTime < meleeAttackSpeed)
-        {
-            elapsedTime += Time.deltaTime;
-            //meleeWeapon.transform.Translate(new Vector3(0, 0, maxMeleeDistance) * meleeAttackSpeed * Time.deltaTime);
-            meleeWeapon.transform.localPosition = Vector3.Lerp(startingPos, startingPos += new Vector3(0, 0, maxMeleeDistance), meleeAttackSpeed);
-            yield return new WaitForEndOfFrame();
-        }
     }
 }
