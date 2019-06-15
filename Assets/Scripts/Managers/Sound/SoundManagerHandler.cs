87f0ae52e2dcb24343875be3277ec2fb5ecd6598
Rui Martins <vandroste@gmail.com>
2019-06-15 18:30:01 +0100

    Add Audio Manager Handler that is referenced in UI buttons and calls Audio Manager, preventing loss of references when reloading scenes

---
+++ Assets/Scripts/Managers/Sound/SoundManagerHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManagerHandler : MonoBehaviour
+{
+    private SoundManager soundManager;
+
+    private void Awake()
+    {
+        soundManager = GameObject.Find("SoundManager").GetComponent<SoundManager>();
+    }
+
+    public void PlaySFX(AudioClip sfx)
+    {
+        soundManager.PlaySFX(sfx);
+    }
+}
