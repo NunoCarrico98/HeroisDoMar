8cc873802dd739dfb978034ea20e4a210461c53c
Rui Martins <vandroste@gmail.com>
2019-06-15 14:31:16 +0100

    Add Audio Manager

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource sfxSource;
+    [SerializeField] private AudioSource musicSource;
+
+    public static SoundManager Instance { get; set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this || Instance != null)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        sfxSource.clip = clip;
+
+        sfxSource.Play();
+    }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        musicSource.clip = clip;
+
+        musicSource.Play();
+    }
+}
